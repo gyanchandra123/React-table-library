1958f9f8768d7538ca58f983dc912d7f1083b632
gyanchandra moirangthem <gyanchandra moirangthem>
2021-09-26 12:39:31 +0530

    pagination1 : containing only 10 default rows

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import FilteringTable from './components/FilteringTable';
+import PaginationTable from './components/PaginationTable';
 
 function App() {
   return (
     <div className="App">
-       <FilteringTable></FilteringTable> 
+       <PaginationTable></PaginationTable> 
     </div>
   );
 }
