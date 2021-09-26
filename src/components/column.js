44db0405e4220e74cfec00d14566589dfe30f20a
gyanchandra moirangthem <gyanchandra moirangthem>
2021-09-26 08:07:51 +0530

    React Table Tutorial - 5 - Header Groups

--- src/components/column.js
+++ src/components/column.js
@@ -1,20 +1,37 @@
-export const COLUMNS = [
-  { Header: "Id",
-    accessor :'id'    
-},
-  { Header: "first Name",
-    accessor:'first_name'  
-},
-  { Header: "last  Name",
-    accessor :'last_name'    
-},
-  { Header: "Date of Birth",
-    accessor:'date_of_birth'    
-},
-  { Header: "Country",
-    accessor:'country'    
-},
-  { Header: "Phone",
-    accessor:'phone'    
-},
+/* export const COLUMNS = [
+  { Header: "Id", accessor: "id", Footer: "Id" },
+  { Header: "first Name", accessor: "first_name", Footer: "first Name" },
+  { Header: "last  Name", accessor: "last_name", Footer: "last  Name" },
+  {
+    Header: "Date of Birth",
+    accessor: "date_of_birth",
+    Footer: "Date of Birth",
+  },
+  { Header: "Country", accessor: "country", Footer: "Country" },
+  { Header: "Phone", accessor: "phone", Footer: "Phone" },
+]; */
+
+export const GROUPED_COLUMNS = [
+  { Header: "Id", accessor: "id", Footer: "Id" },
+  {
+    Header: "name",
+    Footer: "name",
+    columns: [
+      { Header: "first Name", accessor: "first_name", Footer: "first Name" },
+      { Header: "last  Name", accessor: "last_name", Footer: "last  Name" },
+    ],
+  },
+  {
+    Header: "info",
+    Footer: "info",
+    columns: [
+      {
+        Header: "Date of Birth",
+        accessor: "date_of_birth",
+        Footer: "Date of Birth",
+      },
+      { Header: "Country", accessor: "country", Footer: "Country" },
+      { Header: "Phone", accessor: "phone", Footer: "Phone" },
+    ],
+  },
 ];
