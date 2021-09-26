7c5dbab0cc0ac25b64af4dc199dd59f7b75b9499
gyanchandra moirangthem <gyanchandra moirangthem>
2021-09-26 12:49:40 +0530

    pagination1 :adding previous and the next btn

--- src/components/PaginationTable.js
+++ src/components/PaginationTable.js
@@ -1,59 +1,71 @@
-import React,{useMemo,useEffect} from "react";
-import { useTable,usePagination } from "react-table";
-import { COLUMNS, } from "./column";
+import React, { useMemo, useEffect } from "react";
+import { useTable, usePagination } from "react-table";
+import { COLUMNS } from "./column";
 import MOCK_DATA from "./MOCK_DATA.json";
-import './table.css';
+import "./table.css";
 
 const PaginationTable = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
 
-  const columns = useMemo(() => COLUMNS, []) ;
-  const data    = useMemo(()=> MOCK_DATA,[]) ; 
+  const tableInstance = useTable({ columns, data }, usePagination);
 
-  const tableInstance = useTable({columns,data},usePagination); 
+  const {
+    // below are prorerties && functions required from react-table instance.
+    getTableProps, // function
+    getTableBodyProps, //function
+    headerGroups, // array: contains informations regarding the table heading.
+    //rows,
+    page,
+    nextPage,
+    previousPage,
+    prepareRow,
+  } = tableInstance;
 
-  const {  // below are prorerties && functions required from react-table instance.
-      getTableProps, // function
-      getTableBodyProps, //function
-      headerGroups,      // array: contains informations regarding the table heading.
-      //rows, 
-      page,
-      prepareRow
-       } = tableInstance;
+  useEffect(() => {
+    console.log("TABLE-INSTANCE VALUE :", tableInstance);
+    console.log("HEADER-GROUPS VALUE :", headerGroups);
+    console.log("ROWS/page  VALUE :", page);
+  });
 
-       useEffect(() => {
-        console.log('TABLE-INSTANCE VALUE :',tableInstance);
-        console.log('HEADER-GROUPS VALUE :',headerGroups);
-        console.log('ROWS/page  VALUE :',page)
-     });
-     
-       return (
-        <>
-          <table {...getTableProps()}>
-            <thead>
-              {headerGroups.map(headerGroup => ( 
-                <tr {...headerGroup.getHeaderGroupProps()}>
-                  {headerGroup.headers.map(column => (
-                    <th {...column.getHeaderProps()} id={column.id}>{column.render('Header')}</th>
-                  ))}
-                </tr>
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()} id={column.id}>
+                  {column.render("Header")}
+                </th>
               ))}
-            </thead>
-            <tbody {...getTableBodyProps()}>
-              {page.map(row => {
-                prepareRow(row)
-                return (
-                  <tr {...row.getRowProps()} id={row.values.first_name}>
-                    {row.cells.map(cell => {
-                      return <td {...cell.getCellProps()} key={cell.column.id}>{cell.render('Cell')}</td>
-                      /* it picks up the data for each column for each row && render here */
-                    })}
-                  </tr>
-                )
-              })}
-            </tbody> 
-          </table>
-        </>
-      )
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()} id={row.values.first_name}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()} key={cell.column.id}>
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                  /* it picks up the data for each column for each row && render here */
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div>
+        <button onClick={() => previousPage()}>previous</button>
+        <button onClick={() => nextPage()}>next</button>
+      </div>
+    </>
+  );
 };
 
 export default PaginationTable;
